2659f1c5a30416671255a87a72f46b4e2f8283f3
sakib387 <jucse28.387@gamil.com>
2023-10-14 15:51:56 +0600

    category view done

--- EcommerceAPI/product/views.py
+++ EcommerceAPI/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.response import Response
 # Create your views here.
+from .models import Brand,Category,Product
+from .serializers import CategorySerializer,BrandSerializer,ProductSerializer
+
+
+class CategoryView(viewsets.ViewSet):
+    quaryset=Category.objects.all()
+    def list(self,request):
+        serializer=CategorySerializer(self.quaryset,many=True)
+        return Response(serializer.data)
